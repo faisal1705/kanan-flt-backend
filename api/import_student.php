ec805b3443be6361c22b6ea757eeac5dbd401871
faisal1705 <83043849+faisal1705@users.noreply.github.com>
2025-11-28 09:35:34 +0530

    Add CORS support to import_student.php
    Add CORS headers to allow cross-origin requests

--- api/import_student.php
+++ api/import_student.php
@@ -1,4 +1,9 @@
 <?php
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+
 require_once __DIR__ . '/includes/db.php';
 
 // This endpoint receives JSON array from Apps Script and upserts into students table
